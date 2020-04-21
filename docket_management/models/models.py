65774624cbf16437d27c73c07a2f381697c0ff37
Vishal <vishalpachpinde1@gmail.com>
2020-04-21 07:38:15 +0530

    dcoket upgrade

--- docket_management/models/models.py
+++ docket_management/models/models.py
@@ -2,14 +2,7 @@
 
 from odoo import models, fields, api
 
-# class docket_management(models.Model):
-#     _name = 'docket_management.docket_management'
-
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+class Project(models.Model):
+    # _name = "project.project"
+    _description = "Docket"
+    _inherit = 'project.project'
